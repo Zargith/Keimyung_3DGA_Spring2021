5422ae030ddfc7890681943c62a407cbb9102303
Zargith <stanislas.hegron@gmail.com>
2021-05-22 17:57:43 +0900

    Fixed mini_basketball boundaries and removed debug spawn ball button

--- Assets/Scripts/Mini_basketball/mini_basketball_button_object.cs
+++ Assets/Scripts/Mini_basketball/mini_basketball_button_object.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 
 public class mini_basketball_button_object : MonoBehaviour
 {
-    public mini_basketball_GameManager manager;
-    public GameObject topPart;
-    private bool buttonIsPushed = false;
-    private double startPosition;
-    private double endPosition;
-    public float movingSpeed = 0.25f;
+	[SerializeField] mini_basketball_GameManager manager;
+	[SerializeField] GameObject topPart;
+	bool buttonIsPushed = false;
+	double startPosition;
+	double endPosition;
+	[SerializeField] float movingSpeed = 0.25f;
 
-    void Start()
-    {
-        startPosition = topPart.transform.position.y;
-        endPosition = startPosition - 0.085;
-    }
+	void Start()
+	{
+		startPosition = topPart.transform.position.y;
+		endPosition = startPosition - 0.085;
+	}
 
-    void Update()
-    {
-        if (buttonIsPushed)
-            topPart.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
-        else if (!buttonIsPushed && topPart.transform.position.y < startPosition && !manager.gameStarted)
-            topPart.transform.Translate(Vector3.up * Time.deltaTime * movingSpeed);
+	void Update()
+	{
+		if (buttonIsPushed)
+			topPart.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
+		else if (!buttonIsPushed && topPart.transform.position.y < startPosition && !manager.gameStarted)
+			topPart.transform.Translate(Vector3.up * Time.deltaTime * movingSpeed);
 
-        if (topPart.transform.position.y <= endPosition)
-            buttonIsPushed = false;
-    }
+		if (topPart.transform.position.y <= endPosition)
+			buttonIsPushed = false;
+	}
 
 
-    void OnMouseDown() {
-        if (!manager.gameStarted) {
-            buttonIsPushed = true;
-            manager.startGame();
-        }
-    }
+	void OnMouseDown() {
+		if (!manager.gameStarted) {
+			buttonIsPushed = true;
+			manager.startGame();
+		}
+	}
 
-     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("player_hand") && !manager.gameStarted) {
-            buttonIsPushed = true;
-            manager.startGame();
-        }
-    
+	 void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("player_hand") && !manager.gameStarted) {
+			buttonIsPushed = true;
+			manager.startGame();
+		}
+	
 }
 
-    public bool isButtonPushed()
-    {
-        return buttonIsPushed;
-    }
-
+	public bool isButtonPushed()
+	{
+		return buttonIsPushed;
+	}
 }
