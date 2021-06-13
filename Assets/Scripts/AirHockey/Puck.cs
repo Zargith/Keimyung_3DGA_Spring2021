668fa970ded1162f7930ea11ff3d9657a3b0733e
Raykuzu <etienne.g35@gmail.com>
2021-06-13 15:26:11 +0900

    add puck collision sound

--- Assets/Scripts/AirHockey/Puck.cs
+++ Assets/Scripts/AirHockey/Puck.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Puck : MonoBehaviour
 {
     Rigidbody r;
+    AudioSource audioSource;
 
     //// Start is called before the first frame update
     void Start()
     {
         r = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void Stop()
     {
         r.velocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
     }
 
     private void OnTriggerEnter(Collider other)
