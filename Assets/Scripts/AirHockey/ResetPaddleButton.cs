bb37281eba5d077879285820bf747dd1441d39ff
yanis fourel <yanis.fourel@epitech.eu>
2021-06-13 17:08:20 +0900

    Air Hockey : Moved reset paddle button + fixed the velocity reset

--- Assets/Scripts/AirHockey/ResetPaddleButton.cs
+++ Assets/Scripts/AirHockey/ResetPaddleButton.cs
@@ -4,53 +4,62 @@ using UnityEngine;
 
 public class ResetPaddleButton : MonoBehaviour
 {
-	[SerializeField] GameObject playerPaddle;
-	[SerializeField] GameObject topPart;
-	bool buttonIsPushed = false;
-	double startPosition;
-	double endPosition;
-	[SerializeField] float movingSpeed = 0.25f;
-	AudioSource audioSource;
+    [SerializeField] GameObject playerPaddle;
+    [SerializeField] GameObject topPart;
+    bool buttonIsPushed = false;
+    double startPosition;
+    double endPosition;
+    [SerializeField] float movingSpeed = 0.25f;
+    AudioSource audioSource;
 
-	void Start()
-	{
-		startPosition = topPart.transform.position.y;
-		endPosition = startPosition - 0.085;
-		audioSource = GetComponent<AudioSource>();
-	}
+    void Start()
+    {
+        startPosition = topPart.transform.position.y;
+        endPosition = startPosition - 0.085;
+        audioSource = GetComponent<AudioSource>();
+    }
 
-	void Update()
-	{
-		if (buttonIsPushed)
-			topPart.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
-		else if (!buttonIsPushed && topPart.transform.position.y < startPosition)
-			topPart.transform.Translate(Vector3.up * Time.deltaTime * movingSpeed);
+    void Update()
+    {
+        if (buttonIsPushed)
+            topPart.transform.Translate(Vector3.down * Time.deltaTime * movingSpeed);
+        else if (!buttonIsPushed && topPart.transform.position.y < startPosition)
+            topPart.transform.Translate(Vector3.up * Time.deltaTime * movingSpeed);
 
-		if (topPart.transform.position.y <= endPosition)
-			buttonIsPushed = false;
-	}
+        if (topPart.transform.position.y <= endPosition)
+            buttonIsPushed = false;
+    }
 
 
-	void OnMouseDown()
-	{
-		buttonIsPushed = true;
-		audioSource.Play();
-		playerPaddle.transform.position = new Vector3(-0.6f, 0.34f, -0.013f);
-	}
+    void OnMouseDown()
+    {
+        buttonIsPushed = true;
+        audioSource.Play();
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.CompareTag("player_hand"))
-		{
-			buttonIsPushed = true;
-			audioSource.Play();
-			playerPaddle.transform.position = new Vector3(-0.6f, 0.34f, -0.013f);
-		}
+        ResetPlayerPaddle();
+    }
 
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("player_hand"))
+        {
+            buttonIsPushed = true;
+            audioSource.Play();
 
-	public bool isButtonPushed()
-	{
-		return buttonIsPushed;
-	}
+            ResetPlayerPaddle();
+        }
+
+    }
+
+    private void ResetPlayerPaddle()
+    {
+        playerPaddle.transform.position = new Vector3(-0.6f, 0.34f, -0.013f);
+        playerPaddle.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        playerPaddle.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+
+    public bool isButtonPushed()
+    {
+        return buttonIsPushed;
+    }
 }
